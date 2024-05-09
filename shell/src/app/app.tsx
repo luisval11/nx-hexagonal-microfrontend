ea1ef474baa840bd8f477430df36fe02a8d80fb7
Luis Valero <lvalero@dexma.com>
2024-05-09 18:33:50 +0200

    setup(financials): add financials micro frontend

--- shell/src/app/app.tsx
+++ shell/src/app/app.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import NxWelcome from './nx-welcome';
 
 import { Link, Route, Routes } from 'react-router-dom';
+const Financials = React.lazy(() => import('financials/Module'));
 
 export function App() {
   return (
@@ -11,9 +12,13 @@ export function App() {
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/financials">financials</Link>
+        </li>
       </ul>
       <Routes>
-        <Route path="/" element={<NxWelcome title="shell" />} />
+        <Route path="/" element={<NxWelcome title="shell" />} /> 
+        <Route path="/financials" element={<Financials />} />
       </Routes>
     </React.Suspense>
   );
